43c6e05d86ed18833b30e22af45a1fb9c7241528
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2020-01-06 14:32:22 +0100

    Added cartpole example of DDQN, performs amazingly

---
+++ DeepQLearning/cartpole_ddqn.py
@@ -0,0 +1,203 @@
+import gym
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch as T
+from utils import plot_learning_curve
+from replay_memory import ReplayBuffer
+
+class DQNAgent():
+    def __init__(self, gamma, epsilon, lr, n_actions, input_dims, mem_size, batch_size, eps_min=0.01, eps_dec=5e-7, replace=1000, algo=None, env_name=None, chkpt_dir='tmp/dqn'):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.lr = lr
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.batch_size = batch_size
+        self.eps_min = eps_min
+        self.eps_dec = eps_dec
+        self.replace_target_cnt = replace
+        self.algo = algo
+        self.env_name = env_name
+        self.chkpt_dir = chkpt_dir
+        self.action_space = [i for i in range(self.n_actions)]
+        self.learn_step_counter = 0
+
+        self.memory = ReplayBuffer(mem_size, input_dims, n_actions)
+
+        self.q_eval = LinearDeepQNetwork(
+            self.lr, 
+            self.n_actions, 
+            input_dims=self.input_dims
+        )
+
+        self.q_next = LinearDeepQNetwork(
+            self.lr, 
+            self.n_actions, 
+            input_dims=self.input_dims
+        )
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = T.tensor([observation], dtype=T.float32).to(self.q_eval.device)
+            actions = self.q_eval.forward(state)
+            action = T.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def store_transition(self, state, action, reward, state_, done):
+        self.memory.store_transition(state, action, reward, state_, done)
+
+    def sample_memory(self):
+        state, action, reward, new_state, done = self.memory.sample_buffer(self.batch_size)
+
+        states = T.tensor(state).to(self.q_eval.device)
+        rewards = T.tensor(reward).to(self.q_eval.device)
+        dones = T.tensor(done).to(self.q_eval.device)
+        actions = T.tensor(action).to(self.q_eval.device)
+        states_ = T.tensor(new_state).to(self.q_eval.device)
+
+        return states, actions, rewards, states_, dones
+
+    def replace_target_network(self):
+        if self.learn_step_counter % self.replace_target_cnt == 0:
+            self.q_next.load_state_dict(self.q_eval.state_dict())
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
+
+    def save_models(self):
+        self.q_eval.save_checkpoint()
+        self.q_next.save_checkpoint()
+
+    def load_models(self):
+        self.q_eval.load_checkpoint()
+        self.q_next.load_checkpoint()
+
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+        
+        self.q_eval.optimizer.zero_grad()
+
+        self.replace_target_network()
+
+        states, actions, rewards, states_, dones = self.sample_memory()
+
+        indices = np.arange(self.batch_size)
+
+        q_pred = self.q_eval.forward(states)[indices, actions]
+        q_next = self.q_next.forward(states_)
+        q_eval = self.q_eval.forward(states_)
+
+        max_actions = T.argmax(q_eval, dim=1)
+
+        q_next[dones] = 0.0
+
+        q_target = rewards + self.gamma*q_next[indices, max_actions]
+        loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
+        loss.backward()
+
+        self.q_eval.optimizer.step()
+        self.learn_step_counter += 1
+
+        self.decrement_epsilon()
+
+class LinearDeepQNetwork(nn.Module):
+    def __init__(self, lr, n_actions, input_dims):
+        super(LinearDeepQNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(*input_dims, 128)
+        self.fc2 = nn.Linear(128, n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        layer1 = F.relu(self.fc1(state))
+        actions = self.fc2(layer1)
+
+        return actions
+
+class Agent():
+    def __init__(self, input_dims, n_actions, lr, gamma=0.99, epsilon=1.0, eps_dec=1e-5, eps_min=0.01):
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.lr = lr
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_dec = eps_dec
+        self.eps_min = eps_min
+        self.action_space = [i for i in range(self.n_actions)]
+
+        self.Q = LinearDeepQNetwork(self.lr, self.n_actions, self.input_dims)
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = T.tensor(observation, dtype=T.float).to(self.Q.device)
+            actions = self.Q.forward(state)
+            action = T.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+        
+        return action
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
+
+    def learn(self, state, action, reward, state_):
+        self.Q.optimizer.zero_grad()
+        states = T.tensor(state, dtype=T.float).to(self.Q.device)
+        actions = T.tensor(action).to(self.Q.device)
+        rewards = T.tensor(reward).to(self.Q.device)
+        states_ = T.tensor(state_, dtype=T.float).to(self.Q.device)
+
+        q_pred = self.Q.forward(states)[actions]
+
+        q_next = self.Q.forward(states_).max()
+
+        q_target = rewards + self.gamma*q_next
+
+        loss = self.Q.loss(q_target, q_pred).to(self.Q.device)
+        loss.backward()
+        self.Q.optimizer.step()
+        self.decrement_epsilon()
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    n_games = 2500
+    scores = []
+    eps_history = []
+
+    agent = DQNAgent(gamma=0.99, epsilon=1.0, lr=0.0001, input_dims=(env.observation_space.shape), n_actions=env.action_space.n, mem_size=20000, eps_min=0.01, batch_size=32, replace=1000, eps_dec=1e-5, chkpt_dir='models/', algo='DQNAgent', env_name='CartPole-v1')
+    #agent = Agent(lr=0.0001, input_dims=env.observation_space.shape, n_actions=env.action_space.n)
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        obs = env.reset()
+
+        while not done:
+            if i >= 2490:
+                env.render()
+            action = agent.choose_action(obs)
+            obs_, reward, done, info = env.step(action)
+            score += reward
+            agent.store_transition(obs, action, reward, obs_, int(done))
+            agent.learn()
+            obs = obs_
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+
+        if i % 100 == 0:
+            avg_score = np.mean(scores[-100:])
+            print('episode ', i, 'score %.1f avg score %.1f epsilon %.2f' % (score, avg_score, agent.epsilon))
+
+    filename = 'cartpole_naive_dqn.png'
+    x = [i+1 for i in range(n_games)]
+    plot_learning_curve(x, scores, eps_history, filename)
